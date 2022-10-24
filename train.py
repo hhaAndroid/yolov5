96f1c22e68cb93ae4e2922ded642a3b3c37e29d4
huanghaian <huanghaian@sensetime.com>
2022-10-24 16:23:57 +0800

    fix

--- train.py
+++ train.py
@@ -319,6 +319,9 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
                 f"Logging results to {colorstr('bold', save_dir)}\n"
                 f'Starting training for {epochs} epochs...')
     logger.info(f'hyp= {hyp}')
+
+    mloss = torch.zeros(3, device=device)  # mean losses
+
     for epoch in range(start_epoch, epochs):  # epoch ------------------------------------------------------------------
         callbacks.run('on_train_epoch_start')
         model.train()
@@ -333,7 +336,6 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
         # b = int(random.uniform(0.25 * imgsz, 0.75 * imgsz + gs) // gs * gs)
         # dataset.mosaic_border = [b - imgsz, -b]  # height, width borders
 
-        mloss = torch.zeros(3, device=device)  # mean losses
         if RANK != -1:
             train_loader.sampler.set_epoch(epoch)
         # pbar = enumerate(train_loader)
