8b379fbdf2a03ee5b5994206d14a6c147b522e5f
huanghaian <huanghaian@sensetime.com>
2022-12-05 14:48:45 +0800

    new2

--- segment/train.py
+++ segment/train.py
@@ -388,7 +388,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
                 mem = f'{torch.cuda.memory_reserved() / 1E9 if torch.cuda.is_available() else 0:.3g}G'  # (GB)
                 # pbar.set_description(('%10s' * 2 + '%10.4g' * 5) %
                 #                      (f'{epoch}/{epochs - 1}', mem, *mloss, targets.shape[0], imgs.shape[-1]))
-                logger.info(('%10s' * 3 + '%10.4g' * 5) %
+                logger.info(('%10s' * 3 + '%10.4g' * 6) %
                             (
                             f'{i + 1}/{len(train_loader)}', f'{epoch + 1}/{epochs}', mem, *loss_items, targets.shape[0],
                             imgs.shape[-1]))
